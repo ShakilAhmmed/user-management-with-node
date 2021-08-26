8d84572fc2ff2ad319f72edf7a1761d5962f3e82
Shakil Ahmmed <shakil@skylarksoft.com>
2021-08-26 11:35:23 +0600

    User Collection Export

--- router/index.js
+++ router/index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const router = express.Router();
 
-const expressValidator = require('express-validator');
-
 const homeController = require('../controllers/homeController');
 const categoryController = require('../controllers/categoryController');
 
